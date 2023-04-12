288d484b04496d91b6946baf937c420dd7a284f1
Vegard Bolstad <brutelee@me.com>
2023-04-12 11:50:40 +0200

    Minor testing…

--- src/App.svelte
+++ src/App.svelte
@@ -8,13 +8,10 @@
   const leftMenuOpen = writable(false);
   const rightMenuOpen = writable(false);
 
-  function toggleLeftMenu() {
+  function toggleMenu() {
     leftMenuOpen.update(value => !value);
   }
 
-  function toggleRightMenu() {
-    rightMenuOpen.update(value => !value);
-  }
 </script>
 
 <header>
@@ -22,12 +19,12 @@
     <div class="flex items-center justify-between px-4">
       <h1 class="text-lg font-bold">Top menu</h1>
       <div class="flex items-center">
-        <button class="mr-2" on:click={toggleLeftMenu}>
+        <button class="mr-2" on:click={toggleMenu}>
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
           </svg>
         </button>
-        <button on:click={toggleRightMenu}>
+        <button on:click={toggleMenu}>
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
           </svg>
@@ -39,7 +36,7 @@
 
 <div class="flex h-screen w-full">
   <div class="w-0 transition-all duration-300 ease-in-out" class:menu-open={$leftMenuOpen}>
-    <Sidebar position="left" onToggle={toggleLeftMenu} />
+    <Sidebar position="left" onToggle={toggleMenu} />
   </div>
 
   <div class="flex-1 bg-white p-4">
@@ -47,7 +44,7 @@
   </div>
 
   <div class="w-0 transition-all duration-300 ease-in-out" class:menu-open={$rightMenuOpen}>
-    <Sidebar position="right" onToggle={toggleRightMenu} />
+    <Sidebar position="right" onToggle={toggleMenu} />
   </div>
 </div>
 
